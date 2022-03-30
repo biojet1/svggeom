ac7fac66f980c6ccc13d5d241519c5eeba5e383f
biojet1 <biojet1@gmail.com>
2022-03-30 11:33:43 +0800

    PathData LineSegment

--- src/path.ts
+++ src/path.ts
@@ -433,4 +433,4 @@ import {Cubic} from './path/cubic.js';
 import {Quadratic} from './path/quadratic.js';
 export * from './path/describe.js';
 export * from './path/cubic.js';
-export {Arc, Quadratic};
+export {Arc, Quadratic, Line};
