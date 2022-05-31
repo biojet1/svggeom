b3df1c0dc4a6a6aa6eaa6d1a58caffa4b9aacab0
biojet1 <biojet1@gmail.com>
2022-05-31 22:03:45 +0800

    release.yml

---
+++ test/all.mjs
@@ -0,0 +1,7 @@
+import * as all from '../dist/index.js';
+// import * as all from '../dist/document.js';
+// import * as all from '../dist/child-node.js';
+// import * as all from '../dist/parent-node.js';
+// "./parent-node.js"
+console.log(Object.keys(all).sort());
+
