2bcff2cf02ebc958415579bf1df6cf64af171d6f
biojet1 <biojet1@gmail.com>
2022-06-02 02:49:57 +0800

    package.json augment

--- src/path/arc.ts
+++ src/path/arc.ts
@@ -1,11 +1,11 @@
-import {Vec} from '../point.js';
-import {Box} from '../box.js';
-import {SegmentSE} from './index.js';
-import {Line} from './line.js';
-import {Cubic} from './cubic.js';
-import {Matrix} from '../matrix.js';
-import {cossin, unit_vector_angle, segment_length, arcParams, arcToCurve} from '../util.js';
-const {abs, atan, tan, cos, sin, sqrt, acos, atan2, pow, PI, min, max, ceil} = Math;
+import { Vec } from '../point.js';
+import { Box } from '../box.js';
+import { SegmentSE } from './index.js';
+import { Line } from './line.js';
+import { Cubic } from './cubic.js';
+import { Matrix } from '../matrix.js';
+import { segment_length, arcParams, arcToCurve } from '../util.js';
+const { abs, atan, tan, cos, sin,   PI, min, max } = Math;
 
 export class Arc extends SegmentSE {
 	readonly rx: number;
@@ -27,25 +27,16 @@ export class Arc extends SegmentSE {
 		ry: number,
 		φ: number,
 		arc: boolean | number,
-		sweep: boolean | number
+		sweep: boolean | number,
 	) {
 		if (!(isFinite(φ) && isFinite(rx) && isFinite(ry))) throw Error(`${JSON.stringify(arguments)}`);
 		super(start, end);
 
-		const {x: x1, y: y1} = this.start;
-		const {x: x2, y: y2} = this.end;
+		const { x: x1, y: y1 } = this.start;
+		const { x: x2, y: y2 } = this.end;
 
-		[this.phi, this.rx, this.ry, this.sinφ, this.cosφ, this.cx, this.cy, this.rtheta, this.rdelta] = arcParams(
-			x1,
-			y1,
-			rx,
-			ry,
-			φ,
-			(this.arc = !!arc),
-			(this.sweep = !!sweep),
-			x2,
-			y2
-		);
+		[this.phi, this.rx, this.ry, this.sinφ, this.cosφ, this.cx, this.cy, this.rtheta, this.rdelta] =
+			arcParams(x1, y1, rx, ry, φ, (this.arc = !!arc), (this.sweep = !!sweep), x2, y2);
 	}
 	static fromEndPoint(
 		start: Iterable<number>,
@@ -54,7 +45,7 @@ export class Arc extends SegmentSE {
 		φ: number,
 		arc: boolean | number,
 		sweep: boolean | number,
-		end: Iterable<number>
+		end: Iterable<number>,
 	) {
 		if (!rx || !ry) {
 			return new Line(start, end);
@@ -76,7 +67,7 @@ export class Arc extends SegmentSE {
 		return new Arc(start, end, rx, ry, φ, arc, sweep);
 	}
 	bbox() {
-		const {rx, ry, cosφ, sinφ, start, end, rdelta, rtheta, phi} = this;
+		const { rx, ry, cosφ, sinφ, start, end, rdelta, rtheta, phi } = this;
 		let atan_x, atan_y;
 		if (cosφ == 0) {
 			atan_x = PI / 2;
@@ -108,12 +99,12 @@ export class Arc extends SegmentSE {
 		return new Arc(this.start, this.end, this.rx, this.ry, this.phi, this.arc, this.sweep);
 	}
 	get length() {
-		const {start, end} = this;
+		const { start, end } = this;
 		if (start.equals(end)) return 0;
 		return segment_length(this, 0, 1, start, end);
 	}
 	pointAt(t: number) {
-		const {start, end} = this;
+		const { start, end } = this;
 		if (start.equals(end)) {
 			return start.clone();
 		} else if (t <= 0) {
@@ -121,30 +112,45 @@ export class Arc extends SegmentSE {
 		} else if (t >= 1) {
 			return end;
 		}
-		const {rx, ry, cosφ, sinφ, rtheta, rdelta, cx, cy} = this;
+		const { rx, ry, cosφ, sinφ, rtheta, rdelta, cx, cy } = this;
 		const θ = rtheta + rdelta * t;
 		const sinθ = sin(θ);
 		const cosθ = cos(θ);
 		// const [cosθ, sinθ] = cossin((180 * rtheta + 180 * rdelta * t) / PI);
 		// (eq. 3.1) https://svgwg.org/svg2-draft/implnote.html#ArcParameterizationAlternatives
 		try {
-			return Vec.at(rx * cosφ * cosθ - ry * sinφ * sinθ + cx, rx * sinφ * cosθ + ry * cosφ * sinθ + cy);
+			return Vec.at(
+				rx * cosφ * cosθ - ry * sinφ * sinθ + cx,
+				rx * sinφ * cosθ + ry * cosφ * sinθ + cy,
+			);
 		} catch (err) {
 			console.log(rtheta, rdelta, rx, cosφ, cosθ, ry, sinφ, sinθ, cx, cy);
 			throw err;
 		}
 	}
 	splitAt(t: number) {
-		const {rx, ry, phi, sweep, rdelta, start, end} = this;
+		const { rx, ry, phi, sweep, rdelta, start, end } = this;
 		const deltaA = abs(rdelta);
 		const delta1 = deltaA * t;
 		const delta2 = deltaA * (1 - t);
 		const pT = this.pointAt(t);
-		return [new Arc(start, pT, rx, ry, phi, delta1 > PI, sweep), new Arc(pT, end, rx, ry, phi, delta2 > PI, sweep)];
+		return [
+			new Arc(start, pT, rx, ry, phi, delta1 > PI, sweep),
+			new Arc(pT, end, rx, ry, phi, delta2 > PI, sweep),
+		];
 	}
 
 	toPathFragment() {
-		return ['A', this.rx, this.ry, this.phi, this.arc ? 1 : 0, this.sweep ? 1 : 0, this.end.x, this.end.y];
+		return [
+			'A',
+			this.rx,
+			this.ry,
+			this.phi,
+			this.arc ? 1 : 0,
+			this.sweep ? 1 : 0,
+			this.end.x,
+			this.end.y,
+		];
 	}
 	// toString() {
 	// 	return `start: ${this.start.x.toFixed(4)} ${this.start.y.toFixed(
@@ -161,22 +167,25 @@ export class Arc extends SegmentSE {
 	// }
 	slopeAt(t: number): Vec {
 		// throw new Error('Not implemented');
-		const {rx, ry, cosφ, sinφ, rdelta, rtheta} = this;
+		const { rx, ry, cosφ, sinφ, rdelta, rtheta } = this;
 		const θ = rtheta + t * rdelta;
 		const sinθ = sin(θ);
 		const cosθ = cos(θ);
 		const k = rdelta;
 
-		return Vec.at(-rx * cosφ * sinθ * k - ry * sinφ * cosθ * k, -rx * sinφ * sinθ * k + ry * cosφ * cosθ * k);
+		return Vec.at(
+			-rx * cosφ * sinθ * k - ry * sinφ * cosθ * k,
+			-rx * sinφ * sinθ * k + ry * cosφ * cosθ * k,
+		);
 	}
 
 	transform(matrix: any) {
 		// return arc_transform(this, matrix);
-		const {arc, end, start} = this;
-		let {rx, ry, sweep, phi} = this;
+		const { arc, end, start } = this;
+		let { rx, ry, sweep, phi } = this;
 		const p1ˈ = start.transform(matrix);
 		const p2_ = end.transform(matrix);
-		const {rotate, scaleX, scaleY, skewX} = matrix.decompose();
+		const { rotate, scaleX, scaleY, skewX } = matrix.decompose();
 		if (scaleX == scaleY && scaleX != 1) {
 			rx = rx * scaleX;
 			ry = ry * scaleX;
@@ -188,19 +197,19 @@ export class Arc extends SegmentSE {
 			// const M = phi ? matrix.rotate(phi) : matrix;
 			const M = matrix;
 
-			const {a, c, b, d} = M;
+			const { a, c, b, d } = M;
 			const detT = a * d - b * c;
 			const detT2 = detT * detT;
 			if (!rx || !ry || !detT2) break OUT;
 			const A = (d ** 2 / rx ** 2 + c ** 2 / ry ** 2) / detT2;
 			const B = -((d * b) / rx ** 2 + (c * a) / ry ** 2) / detT2;
 			const D = (b ** 2 / rx ** 2 + a ** 2 / ry ** 2) / detT2;
-			const theta = atan2(-2 * B, D - A) / 2;
+			// const theta = atan2(-2 * B, D - A) / 2;
 			// console.log('theta', theta_deg, theta, A, B, D, a, c, b, d, detT2);
 			const DA = D - A;
-			const l2 = 4 * B ** 2 + DA ** 2;
-			const delta = l2 ? (0.5 * (-DA * DA - 4 * B * B)) / sqrt(l2) : 0;
-			const half = (A + D) / 2;
+			// const l2 = 4 * B ** 2 + DA ** 2;
+			// const delta = l2 ? (0.5 * (-DA * DA - 4 * B * B)) / sqrt(l2) : 0;
+			// const half = (A + D) / 2;
 			// if (skewX || scaleX != 1 || scaleY != 1) {
 			// 	rx = 1.0 / sqrt(half + delta);
 			// 	ry = 1.0 / sqrt(half - delta);
@@ -267,19 +276,19 @@ export class Arc extends SegmentSE {
 	}
 
 	reversed() {
-		const {arc, end, start, rx, ry, sweep, phi} = this;
+		const { arc, end, start, rx, ry, sweep, phi } = this;
 		return new Arc(end, start, rx, ry, phi, arc, sweep ? 0 : 1);
 	}
 
 	asCubic() {
 		const {
-			arc,
-			end: {x: x2, y: y2},
-			start: {x: x1, y: y1},
+			// arc,
+			end: { x: x2, y: y2 },
+			start: { x: x1, y: y1 },
 			rx,
 			ry,
-			sweep,
-			phi,
+			// sweep,
+			// phi,
 			cx,
 			cy,
 			cosφ,
