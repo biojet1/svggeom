66dcfbdba9f477b7bfcc7d7bf2f3339a756b7c4d
biojet1 <biojet1@gmail.com>
2024-08-26 04:55:02 +0800

    dropped PathLS

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,10 @@ export * from './ray.js';
 export * from './bbox.js';
 export * from './matrix.js';
 export * from './svgtransform.js';
-// export { PathSE } from './path/segment/pathse.js';
-import { PathLC } from './path/pathcl.js';
-
+import { PathLC } from './path/pathlc.js';
 const { Unit } = PathLC;
 export { PathLC as PathLS, Unit as SegmentLS }
-
-export { PathLC } from './path/pathcl.js';
+export { PathLC } from './path/pathlc.js';
 export async function loadFont(which: string) {
 	return import('./font.js').then(mod => mod.FontCache.getInstance().getFont(which));
 }
--- src/path/index.ts
+++ src/path/index.ts
@@ -1,6 +1,3 @@
-import { Vector } from '../vector.js';
-import { BoundingBox } from '../bbox.js';
-
 export interface DescParams {
 	relative?: boolean;
 	smooth?: boolean;
@@ -9,30 +6,6 @@ export interface DescParams {
 	dfix?: number;
 }
 
-export abstract class Segment {
-	abstract get from(): Vector;
-	abstract get to(): Vector;
-	abstract get length(): number;
-	abstract bbox(): BoundingBox;
-	abstract point_at(t: number): Vector;
-	abstract slope_at(t: number): Vector;
-
-	tangent_at(t: number) {
-		const vec = this.slope_at(t);
-		return vec.div(vec.abs());
-	}
-	toPath(): string {
-		const { x, y } = this.from;
-		return ['M', x, y].concat(this.toPathFragment()).join(' ');
-	}
-	terms(opt?: DescParams): (string | number)[] {
-		const { x, y } = this.from;
-		return ['M', x, y].concat(this.toPathFragment(opt));
-	}
-	toPathFragment(opt?: DescParams): (string | number)[] {
-		throw new Error('NOTIMPL');
-	}
-}
 
 export function tCheck(t: number) {
 	if (t > 1) {
@@ -55,46 +28,4 @@ export function tNorm(t: number) {
 		}
 	}
 	return t;
-}
-
-export function* pickPos(args: Iterable<number>[] | number[] | Iterable<number>[]) {
-	let n: number | undefined = undefined;
-	for (const v of args) {
-		if (typeof v == 'number') {
-			if (n == undefined) {
-				n = v;
-			} else {
-				yield Vector.new(n, v);
-				n = undefined;
-			}
-		} else if (n != undefined) {
-			throw new Error(`n == ${n}`);
-		} else if (v instanceof Vector) {
-			yield v;
-		} else {
-			yield Vector.new(v);
-		}
-	}
-}
-
-export function* pickNum(args: Iterable<number>[] | number[]) {
-	for (const v of args) {
-		switch (typeof v) {
-			case 'number':
-				yield v;
-				break;
-			case 'boolean':
-			case 'string':
-				yield v ? 1 : 0;
-				break;
-			default:
-				if (v) {
-					const [x, y] = v;
-					yield x;
-					yield y;
-				} else {
-					yield 0;
-				}
-		}
-	}
 }
